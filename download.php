3ec73c205e504f55851be83d323c5e1d209917d8
Bohan Yang <youthdna@live.com>
2019-11-10 18:26:25 +0900

    Add S3

--- download.php
+++ download.php
@@ -8,6 +8,7 @@ use BohanCo\BingHomepageImage\Downloader;
 use BohanCo\BingHomepageImage\LeanCloudRepository;
 use BohanCo\BingHomepageImage\LeanCloudSDK\ArrayStorage;
 use LeanCloud\Client;
+use LeanCloud\User;
 
 Client::initialize(
     getenv('LEANCLOUD_APP_ID'),
@@ -32,6 +33,13 @@ $images = $repository->unreadyImages();
 
 if (!empty($images)) {
     $downloader = new Downloader(getenv('DEST_DIR'));
-    $downloader->download($images);
+    $downloader->download($images, [
+        getenv('AWS_ACCESS_KEY_ID'),
+        getenv('AWS_SECRET_ACCESS_KEY'),
+        getenv('S3_ENDPOINT'),
+        getenv('S3_BUCKET'),
+        getenv('S3_FOLDER'),
+        getenv('AWS_DEFAULT_REGION'),
+    ]);
     $repository->setImagesReady(array_keys($images));
 }
