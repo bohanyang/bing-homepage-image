d71535f02faf3ad008f1247ec6d16e10349d16de
Bohan Yang <youthdna@live.com>
2020-03-05 01:20:39 +0900

    Fix regex

--- src/BingClient.php
+++ src/BingClient.php
@@ -161,7 +161,7 @@ final class BingClient
      */
     public static function parseCopyright(string $copyright) : array
     {
-        $regex = '/(.+?)(?: |\x{3000})?(?:\(|\x{FF08})?\x{00A9}(?: |\x{3000})?(.+)(?:\)|\x{FF09})/u';
+        $regex = '/(.+?)(?: |\x{3000})?(?:\(|\x{FF08})?\x{00A9}(?: |\x{3000})?(.+?)(?:\)|\x{FF09})?$/u';
         $matches = [];
 
         if (preg_match($regex, $copyright, $matches) !== 1) {
