13aa4a0354275f2ad9a0b65f806be5f77e1eb788
Bohan Yang <youthdna@live.com>
2021-03-06 14:11:52 +0000

    Add replicate

--- download.php
+++ download.php
@@ -9,6 +9,7 @@ use BohanYang\BingWallpaper\Downloader;
 use BohanYang\BingWallpaper\LeanCloudRepository;
 use BohanYang\BingWallpaper\LeanCloudSDK\ArrayStorage;
 use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Flysystem\Replicate\ReplicateAdapter;
 use League\Flysystem\Filesystem;
 use LeanCloud\Client;
 use LeanCloud\User;
@@ -41,7 +42,7 @@ $repository = new LeanCloudRepository();
 $images = $repository->unreadyImages();
 
 if (!empty($images)) {
-    $client = new S3Client([
+    $client1 = new S3Client([
         'credentials' => [
             'key'    => getenv('AWS_ACCESS_KEY_ID'),
             'secret' => getenv('AWS_SECRET_ACCESS_KEY'),
@@ -52,11 +53,28 @@ if (!empty($images)) {
         'bucket_endpoint' => false,
         'use_path_style_endpoint' => true,
     ]);
-    $adapter = new AwsS3Adapter($client, getenv('S3_BUCKET'), getenv('S3_FOLDER'), [
+    $client2 = new S3Client([
+        'credentials' => [
+            'key'    => getenv('AWS_ACCESS_KEY_ID_2'),
+            'secret' => getenv('AWS_SECRET_ACCESS_KEY_2'),
+        ],
+        'endpoint' => getenv('S3_ENDPOINT_2'),
+        'region' => getenv('AWS_DEFAULT_REGION_2'),
+        'version' => 'latest',
+        'bucket_endpoint' => false,
+        'use_path_style_endpoint' => true,
+    ]);
+    $adapter1 = new AwsS3Adapter($client1, getenv('S3_BUCKET'), getenv('S3_FOLDER'), [
         'CacheControl' => 'max-age=31536000',
         'ContentType' => 'image/jpeg',
         'ACL' => ''
     ]);
+    $adapter2 = new AwsS3Adapter($client2, getenv('S3_BUCKET_2'), getenv('S3_FOLDER_2'), [
+        'CacheControl' => 'max-age=31536000',
+        'ContentType' => 'image/jpeg',
+        'ACL' => 'public-read'
+    ]);
+    $adapter = new ReplicateAdapter($adapter1, $adapter2);
     $downloader = new Downloader(new Filesystem($adapter));
     $downloader->download($images);
     $repository->setImagesReady(array_keys($images));
